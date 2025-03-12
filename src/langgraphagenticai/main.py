3b1d5fe1595a825a792908bef3b9bd025776aa10
kamaleswar Mohanta <88280766+Icamtu@users.noreply.github.com>
2025-03-12 11:33:03 +0530

    Basic Chatbot

--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import json
 from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
-# from src.langgraphagenticai.LLMS.groqllm import GroqLLM
-# from src.langgraphagenticai.graph.graph_builder import GraphBuilder
-# from src.langgraphagenticai.ui.streamlitui.display_result import DisplayResultStreamlit
+from src.langgraphagenticai.LLMS.groqllm import GroqLLM
+from src.langgraphagenticai.graph.graph_builder import GraphBuilder
+from src.langgraphagenticai.ui.streamlitui.display_result import DisplayResultStreamlit
 
 # MAIN Function START
 def load_langgraph_agenticai_app():
@@ -28,32 +28,32 @@ def load_langgraph_agenticai_app():
     else :
         user_message = st.chat_input("Enter your message:")
 
-    # if user_message:
-    #         try:
-    #             # Configure LLM
-    #             obj_llm_config = GroqLLM(user_controls_input=user_input)
-    #             model = obj_llm_config.get_llm_model()
+    if user_message:
+            try:
+                # Configure LLM
+                obj_llm_config = GroqLLM(user_controls_input=user_input)
+                model = obj_llm_config.get_llm_model()
                 
-    #             if not model:
-    #                 st.error("Error: LLM model could not be initialized.")
-    #                 return
+                if not model:
+                    st.error("Error: LLM model could not be initialized.")
+                    return
 
-    #             # Initialize and set up the graph based on use case
-    #             usecase = user_input.get('selected_usecase')
-    #             if not usecase:
-    #                 st.error("Error: No use case selected.")
-    #                 return
-                
-
-    #             ### Graph Builder
-    #             graph_builder=GraphBuilder(model)
-    #             try:
-    #                 graph = graph_builder.setup_graph(usecase)
-    #                 DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
-    #             except Exception as e:
-    #                 st.error(f"Error: Graph setup failed - {e}")
-    #                 return
+                # Initialize and set up the graph based on use case
+                usecase = user_input.get('selected_usecase')
+                if not usecase:
+                    st.error("Error: No use case selected.")
+                    return
                 
 
-    #         except Exception as e:
-    #              raise ValueError(f"Error Occurred with Exception : {e}")
+                ### Graph Builder
+                graph_builder=GraphBuilder(model)
+                try:
+                    graph = graph_builder.setup_graph(usecase)
+                    DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
+                except Exception as e:
+                    st.error(f"Error: Graph setup failed - {e}")
+                    return
+                
+
+            except Exception as e:
+                 raise ValueError(f"Error Occurred with Exception : {e}")
