9a53e207350fcda7bf73b95d232c5bf30bc16b56
kamaleswar Mohanta <88280766+Icamtu@users.noreply.github.com>
2025-03-19 01:01:29 +0530

    Remove commented-out code and clean up imports in state.py

--- src/langgraphagenticai/state/state.py
+++ src/langgraphagenticai/state/state.py
@@ -1,15 +1,3 @@
-# from typing import Annotated, Literal, Optional, List
-# from typing_extensions import TypedDict
-# from langgraph.graph.message import add_messages
-# from langchain_core.messages import HumanMessage, AIMessage
-
-# class State(TypedDict):
-#     """
-#     Represents the structure of the state used in the graph.
-#     """
-#     messages: Annotated[list,add_messages]
-
-# state.py
 from typing import Annotated, List, Optional
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
